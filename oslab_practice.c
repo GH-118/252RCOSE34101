4cd642247143d02d7e030993b053677eded421f0
GH-118 <imchangmin0118@gmail.com>
2025-12-10 22:54:51 +0900

    Add print_numbers function

--- oslab_practice.c
+++ oslab_practice.c
@@ -4,7 +4,16 @@ void print_header(void) {
     printf("==== OS Lab Git Practice ====\n");
 }
 
+void print_numbers(int n) {
+    printf("Numbers from 1 to %d:\n", n);
+    for (int i = 1; i <= n; i++) {
+        printf("%d ", i);
+    }
+    printf("\n");
+}
+
 int main(void) {
     print_header();
+    print_numbers(5);
     return 0;
 }
